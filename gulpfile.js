0584ea2ad7624bd86a2fa8f0ecd6a26cb6e8705b
Jake Lambourn <jlambourn@vertical-leap.uk>
2022-05-19 10:18:06 +0100

    Git init

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+const {
+    src,
+    dest,
+    parallel,
+    series,
+    watch
+} = require('gulp');
+
+// Load plugins
+
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
+const imagemin = require('gulp-imagemin');
+const changed = require('gulp-changed');
+const browsersync = require('browser-sync').create();
+const gulpESLintNew = require('gulp-eslint-new');
+const sassLint = require('gulp-sass-lint');
+
+// Clean assets
+
+function clear() {
+    return src('./assets/*', {
+            read: false
+        })
+        .pipe(clean());
+}
+
+// JS function
+
+function js() {
+    const source = './src/js/*.js';
+
+    return src(source)
+        .pipe(changed(source))
+		.pipe(gulpESLintNew({ fix: true }))         // Lint files, create fixes.
+	    .pipe(gulpESLintNew.fix())                  // Fix files if necessary.
+	    .pipe(gulpESLintNew.format())               // Outputs lint results to the console.
+	    .pipe(gulpESLintNew.failAfterError())
+        .pipe(concat('bundle.js'))
+        .pipe(uglify())
+        .pipe(rename({
+            extname: '.min.js'
+        }))
+        .pipe(dest('./assets/js/'))
+        .pipe(browsersync.stream());
+}
+
+// CSS function
+
+function css() {
+    const source = './src/scss/main.scss';
+
+    return src(source)
+        .pipe(changed(source))
+        .pipe(sass())
+		.pipe(sassLint())
+	    .pipe(sassLint.format())
+	    .pipe(sassLint.failOnError())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(cssnano())
+        .pipe(dest('./assets/css/'))
+        .pipe(browsersync.stream());
+}
+
+// Optimize images
+
+function img() {
+    return src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(dest('./assets/img'));
+}
+
+// Watch files
+
+function watchFiles() {
+    watch('./src/scss/*', css);
+    watch('./src/js/*', js);
+    watch('./src/img/*', img);
+}
+
+// BrowserSync
+
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: './'
+        },
+        port: 3000
+    });
+}
+
+// Tasks to define the execution of the functions simultaneously or in series
+
+exports.watch = parallel(watchFiles, browserSync);
+exports.default = series(clear, parallel(js, css, img));
